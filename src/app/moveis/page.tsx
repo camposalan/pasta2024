e94ad150ae0a8aed22b6ceb74310f9aea6fd3e99
Alan Campos <camposalan@hotmail.com>
2025-01-07 14:55:24 -0300

    alteracao php

--- src/app/moveis/page.tsx
+++ src/app/moveis/page.tsx
@@ -1,5 +1,7 @@
 export default function Moveis(){
     return(
-        <h1 className="text-amber-400">Em manutenção</h1>
+        <h1 className="text-amber-400">Consulte</h1>
+        <p>github.com/camposalan/filmesphp</p>
+        <p>github.com/camposalan/monster-burguer</p>
     )
 }
